3077cc171dccfa9b72c99464eb3d4f594746a686
Alan Coopersmith <alan.coopersmith@oracle.com>
2025-07-27 12:09:28 -0700

    vInitChrontelReg: drop unused variable pEnhModePtr
    ast_mode.c: In function 'vInitChrontelReg':
    ast_mode.c:1287:28: warning: unused variable 'pEnhModePtr' [-Wunused-variable]
     1287 |     PVBIOS_ENHTABLE_STRUCT pEnhModePtr = pVGAModeInfo->pEnhTableEntry;
          |                            ^~~~~~~~~~~
    
    Fixes: 9e8d8489922d8b647b28de06045e8fddf0ffc62f
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-ast/-/merge_requests/15>

--- src/ast_mode.c
+++ src/ast_mode.c
@@ -1284,8 +1284,6 @@ static UCHAR GetChrontelReg(ASTRecPtr pAST, UCHAR jChannel, UCHAR jIndex)
 
 static void vInitChrontelReg(ScrnInfoPtr pScrn, DisplayModePtr mode, PVBIOS_MODE_INFO pVGAModeInfo)
 {
-
-    PVBIOS_ENHTABLE_STRUCT pEnhModePtr = pVGAModeInfo->pEnhTableEntry;
     ASTRecPtr pAST = ASTPTR(pScrn);
     ULONG ulDCLK = 65;					/* todo */
     UCHAR jReg;
