X11Libre/xf86-video-ast

commit 77e7ac37c6031551f7ad355dc94c4176df4956aa

use same search mode criteria with ast drm driver

src/ast_mode.c
@@ -495,6 +495,8 @@ static Bool bGetAST1000VGAModeInfo(ScrnInfoPtr pScrn, DisplayModePtr mode, PVBIO
     ASTRecPtr pAST;
     ULONG ulModeID, ulColorIndex, ulRefreshRate, ulRefreshRateIndex = 0;
     ULONG ulHBorder, ulVBorder;
+    Bool check_sync;
+    PVBIOS_ENHTABLE_STRUCT loop, best = NULL;
 
     pAST = ASTPTR(pScrn);
 
@@ -560,27 +562,34 @@ static Bool bGetAST1000VGAModeInfo(ScrnInfoPtr pScrn, DisplayModePtr mode, PVBIO
     }
 
     /* Get Proper Mode Index */
-    if (pVGAModeInfo->pEnhTableEntry->Flags & WideScreenMode)
-    {
-        /* parsing for wide screen reduced blank mode */
-        if ((mode->Flags & V_NVSYNC) && (mode->Flags & V_PHSYNC))	/* CVT RB */
-            pVGAModeInfo->pEnhTableEntry++;
-    }
-    else
-    {
-        ulRefreshRate = (mode->Clock * 1000) / (mode->HTotal * mode->VTotal);
-
-        while (pVGAModeInfo->pEnhTableEntry->ulRefreshRate < ulRefreshRate)
-        {
- 	        pVGAModeInfo->pEnhTableEntry++;
-	        if ((pVGAModeInfo->pEnhTableEntry->ulRefreshRate > ulRefreshRate) ||
-	           (pVGAModeInfo->pEnhTableEntry->ulRefreshRate == 0xFF))
-	        {
-	            pVGAModeInfo->pEnhTableEntry--;
-	            break;
-	        }
-        }
-    }
+    ulRefreshRate = (mode->Clock * 1000) / (mode->HTotal * mode->VTotal) + 1;
+    loop = pVGAModeInfo->pEnhTableEntry;
+    check_sync = loop->Flags & WideScreenMode;
+    do {
+	while (loop->ulRefreshRate != 0xff) {
+		if ((check_sync) &&
+		    (((mode->Flags & V_NVSYNC)  &&
+		      (loop->Flags & PVSync))  ||
+		     ((mode->Flags & V_PVSYNC)  &&
+		      (loop->Flags & NVSync))  ||
+		     ((mode->Flags & V_NHSYNC)  &&
+		      (loop->Flags & PHSync))  ||
+		     ((mode->Flags & V_PHSYNC)  &&
+		      (loop->Flags & NHSync)))) {
+			loop++;
+			continue;
+		}
+	if (loop->ulRefreshRate <= ulRefreshRate
+		    && (!best || loop->ulRefreshRate > best->ulRefreshRate))
+			best = loop;
+			loop++;
+	}
+	if (best || !check_sync)
+		break;
+	check_sync = 0;
+    } while (1);
+    if (best)
+	pVGAModeInfo->pEnhTableEntry = best;
 
     /* Update mode CRTC info */
     ulHBorder = (pVGAModeInfo->pEnhTableEntry->Flags & HBorder) ? 8:0;

src/ast_mode.h
@@ -66,14 +66,18 @@
 #define HalfDCLK                0x00000002
 #define DoubleScanMode          0x00000004
 #define LineCompareOff          0x00000008
-#define SyncPP                  0x00000000
-#define SyncPN                  0x00000040
-#define SyncNP                  0x00000080
-#define SyncNN                  0x000000C0
 #define HBorder                 0x00000020
 #define VBorder                 0x00000010
-#define WideScreenMode		    0x00000100
-#define NewModeInfo		        0x00000200
+#define WideScreenMode		0x00000100
+#define NewModeInfo		0x00000200
+#define NHSync			0x00000400
+#define PHSync			0x00000800
+#define NVSync			0x00001000
+#define PVSync			0x00002000
+#define	SyncPP			(PVSync | PHSync)
+#define	SyncPN			(PVSync | NHSync)
+#define	SyncNP			(NVSync | PHSync)
+#define	SyncNN			(NVSync | NHSync)
 
 /* DAC Definition */
 #define DAC_NUM_TEXT		64

commit 63df979a02f27a15c2813738dd2b228a2a061f05

bGetAST1000VGAModeInfo: Fix misleading indentation
ast_mode.c: In function ‘bGetAST1000VGAModeInfo’:
ast_mode.c:647:9: warning: this ‘if’ clause does not guard...
 [-Wmisleading-indentation]
  647 |         if (loop->ulRefreshRate <= ulRefreshRate
      |         ^~
ast_mode.c:650:25: note: ...this statement, but the latter is
 misleadingly indented as if it were guarded by the ‘if’
  650 |                         loop++;
      |                         ^~~~

Fixes: 77e7ac3 ("use same search mode criteria with ast drm driver")
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-ast/-/merge_requests/15>

src/ast_mode.c
@@ -644,10 +644,10 @@ static Bool bGetAST1000VGAModeInfo(ScrnInfoPtr pScrn, DisplayModePtr mode, PVBIO
 			loop++;
 			continue;
 		}
-	if (loop->ulRefreshRate <= ulRefreshRate
+		if (loop->ulRefreshRate <= ulRefreshRate
 		    && (!best || loop->ulRefreshRate > best->ulRefreshRate))
 			best = loop;
-			loop++;
+		loop++;
 	}
 	if (best || !check_sync)
 		break;